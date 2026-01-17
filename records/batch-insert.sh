ca905c180381d12182796981cb86c3335c246cfa
Terry Parrish <heymonheymon3000@yahoo.com>
2026-01-17 08:13:14 -0500

    Integrate API Gateway with React front-emd

---
+++ records/batch-insert.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+aws dynamodb batch-write-item --request-items file://records-1.json
+aws dynamodb batch-write-item --request-items file://records-2.json
+aws dynamodb batch-write-item --request-items file://records-3.json
+aws dynamodb batch-write-item --request-items file://records-4.json
